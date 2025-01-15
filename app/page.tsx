5599eeaf609b1237e0a8fdbca677381c2940ac50
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-01-15 18:29:36 +0530

    added image carousel component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { options } from "./api/auth/[...nextauth]/options"
 import { getServerSession } from "next-auth/next"
 import UserCard from "./components/UserCard"
+import ImageSlider from "./components/ImageSlider"
 
 export default async function Home() {
   const session = await getServerSession(options)
 
   return (
     <>
+    <ImageSlider/>
       {session ? (
         <UserCard user={session?.user} pagetype={"Home"} />
       ) : (
